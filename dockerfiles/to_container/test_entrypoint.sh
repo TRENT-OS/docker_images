8f8222fdf6be818a0a19ee1b324b86e3b95e1dc0
Thomas Böhm <thomas.boehm@hensoldt-cyber.com>
2020-07-01 16:57:30 +0200

    [DEV] Start services in container from entrypoint script

--- dockerfiles/to_container/test_entrypoint.sh
+++ dockerfiles/to_container/test_entrypoint.sh
@@ -4,5 +4,9 @@ set -euxo pipefail
 
 sudo /bin/bash /tmp/test_setup_internal_network.sh > /dev/null 2>&1
 
+# start servives in container
+sudo service nginx start
+sudo service mosquitto start
+
 # execute the command that was passed to docker run
 exec "$@"
