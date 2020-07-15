6de7f8ece5acd98ff3de5608305fcde858d08c08
Thomas Böhm <thomas.boehm@hensoldt-cyber.com>
2020-07-15 14:36:49 +0200

    [DEV] Set debian version to install from

--- dockerfiles/to_container/trentos_build_env.sh
+++ dockerfiles/to_container/trentos_build_env.sh
@@ -28,8 +28,7 @@ PACKAGES=(
 )
 
 DEBIAN_FRONTEND=noninteractive apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
-DEBIAN_FRONTEND=noninteractive apt-get --no-install-recommends install -y ${PACKAGES[@]}
+DEBIAN_FRONTEND=noninteractive apt-get install -t bullseye --no-install-recommends  -y ${PACKAGES[@]}
 DEBIAN_FRONTEND=noninteractive apt-get clean autoclean
 DEBIAN_FRONTEND=noninteractive apt-get autoremove --yes
 
