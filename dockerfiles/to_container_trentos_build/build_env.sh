b601037a7c2680e680c3b36fedae5a5f4d7d5317
Thomas Böhm <thomas.boehm@hensoldt-cyber.de>
2021-01-28 11:27:22 +0100

    [DEV] Add user to the stack group

--- dockerfiles/to_container_trentos_build/build_env.sh
+++ dockerfiles/to_container_trentos_build/build_env.sh
@@ -12,6 +12,8 @@ adduser ${USER_NAME} sudo
 passwd -d ${USER_NAME}
 chown -R ${USER_NAME}:${USER_NAME} /home/${USER_NAME}
 chmod -R ug+rw /home/${USER_NAME}
+# add user to the stack group to access the pre-installed haskell toolchain
+usermod -a -G stack ${USER_NAME}
 
 echo 'export PATH=/scripts/repo:$PATH' >> /home/${USER_NAME}/.bashrc
 
