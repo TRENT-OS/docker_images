396f8985ae3b49c806bfd8ba80f94298661786e3
Franz Schauer <franz.schauer@hensoldt-cyber.de>
2021-05-11 16:21:28 +0200

    [DEV] install and prepare sshfs in analysis container

--- dockerfiles/to_container_trentos_analysis/analysis_env.sh
+++ dockerfiles/to_container_trentos_analysis/analysis_env.sh
@@ -2,10 +2,19 @@
 
 set -euxo pipefail
 
+USER_ID="$1"
+USER_NAME="$2"
+
+# install required packages
 PACKAGES=(
+    sshfs
 )
 
 DEBIAN_FRONTEND=noninteractive apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -t bullseye --no-install-recommends  -y ${PACKAGES[@]}
 DEBIAN_FRONTEND=noninteractive apt-get clean autoclean
 DEBIAN_FRONTEND=noninteractive apt-get autoremove --yes
+
+# setup group for sshfs
+groupadd fuse
+usermod -a -G fuse ${USER_NAME}
