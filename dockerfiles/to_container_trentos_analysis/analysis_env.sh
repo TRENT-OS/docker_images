9a0c307ce0b42c4733df56831694f54ec696d906
Franz Schauer <franz.schauer@hensoldt-cyber.de>
2021-05-17 17:49:21 +0200

    [DEV] get and install axivion suite in analysis container

--- dockerfiles/to_container_trentos_analysis/analysis_env.sh
+++ dockerfiles/to_container_trentos_analysis/analysis_env.sh
@@ -25,3 +25,20 @@ rm -rf /var/lib/apt/lists/*
 # setup group for sshfs
 groupadd fuse
 usermod -a -G fuse ${USER_NAME}
+
+# get and install axivion suite
+wget --no-check-certificate https://hc-artefact/axivion_suite/bauhaus-suite-7_1_5-x86_64-gnu_linux.tar.gz -O /opt/bauhaus-suite.tar.gz
+
+if ! echo "72ff69be0728cccbcb45190b5899814a2e96a23f69352c4434dc60290d4f6458 /opt/bauhaus-suite.tar.gz" | sha256sum -c -; then
+     echo "Hash of bauhaus-suite.tar.gz invalid"
+     exit 1
+fi
+
+cd /opt
+tar xzf bauhaus-suite.tar.gz
+rm bauhaus-suite.tar.gz
+
+cd bauhaus-suite
+./setup.sh
+
+echo 'export PATH=/opt/bauhaus-suite/bin:$PATH' >> /home/${USER_NAME}/.bashrc
