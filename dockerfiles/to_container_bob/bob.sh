d46a086ad256e709ca6dca97eb4f750213a2277b
Axel Heider <axel.heider@hensoldt.net>
2022-06-18 15:38:02 +0200

    [DEV] check hash of downloaded archive first

--- dockerfiles/to_container_bob/bob.sh
+++ dockerfiles/to_container_bob/bob.sh
@@ -58,13 +58,13 @@ ln -s /opt/cmake/bin/ctest     /usr/local/bin/ctest
 rm /tmp/cmake.sh
 
 # install fixuid to fix the runtime UID/GID problem in the container entrypoint script
-wget https://github.com/boxboat/fixuid/releases/download/v0.5/fixuid-0.5-linux-amd64.tar.gz -O /tmp/fixuid-0.5-linux-amd64.tar.gz
-tar -C /usr/local/bin -xzf /tmp/fixuid-0.5-linux-amd64.tar.gz
-if ! echo "caa7e0e4c88e1b154586a46c2edde75a23f9af4b5526bb11626e924204585050 /tmp/fixuid-0.5-linux-amd64.tar.gz" | sha256sum -c -; then
+wget https://github.com/boxboat/fixuid/releases/download/v0.5/fixuid-0.5-linux-amd64.tar.gz -O /tmp/fixuid.gz
+if ! echo "caa7e0e4c88e1b154586a46c2edde75a23f9af4b5526bb11626e924204585050 /tmp/fixuid.gz" | sha256sum -c -; then
      echo "Hash failed"
      exit 1
 fi
-rm /tmp/fixuid-0.5-linux-amd64.tar.gz
+tar -C /usr/local/bin -xzf /tmp/fixuid.gz
+rm /tmp/fixuid.gz
 chown root:root /usr/local/bin/fixuid
 chmod 4755 /usr/local/bin/fixuid
 mkdir -p /etc/fixuid
--- dockerfiles/to_container_trentos_test/test_env.sh
+++ dockerfiles/to_container_trentos_test/test_env.sh
@@ -69,13 +69,13 @@ echo "Set disable_coredump false" >> /etc/sudo.conf
 ln -s /usr/bin/pytest-3 /usr/bin/pytest
 
 # install fixuid to fix the runtime UID/GID problem in the container entrypoint script
-wget https://github.com/boxboat/fixuid/releases/download/v0.5/fixuid-0.5-linux-amd64.tar.gz -O /tmp/fixuid-0.5-linux-amd64.tar.gz
-tar -C /usr/local/bin -xzf /tmp/fixuid-0.5-linux-amd64.tar.gz
-if ! echo "caa7e0e4c88e1b154586a46c2edde75a23f9af4b5526bb11626e924204585050 /tmp/fixuid-0.5-linux-amd64.tar.gz" | sha256sum -c -; then
+wget https://github.com/boxboat/fixuid/releases/download/v0.5/fixuid-0.5-linux-amd64.tar.gz -O /tmp/fixuid.gz
+if ! echo "caa7e0e4c88e1b154586a46c2edde75a23f9af4b5526bb11626e924204585050 /tmp/fixuid.gz" | sha256sum -c -; then
      echo "Hash failed"
      exit 1
 fi
-rm /tmp/fixuid-0.5-linux-amd64.tar.gz
+tar -C /usr/local/bin -xzf /tmp/fixuid.gz
+rm /tmp/fixuid.gz
 chown root:root /usr/local/bin/fixuid
 chmod 4755 /usr/local/bin/fixuid
 mkdir -p /etc/fixuid
