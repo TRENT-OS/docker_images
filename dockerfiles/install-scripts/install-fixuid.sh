65c07f70d7bd08fc228cb374793904bdc35e53d6
Axel Heider <axel.heider@hensoldt.net>
2022-08-18 13:29:14 +0200

    [DEV] update fixuid to v0.5.1

--- dockerfiles/install-scripts/install-fixuid.sh
+++ dockerfiles/install-scripts/install-fixuid.sh
@@ -4,8 +4,8 @@ USER_ID="$1"
 USER_NAME="$2"
 
 # install fixuid to fix the runtime UID/GID problem in the container entrypoint script
-wget https://github.com/boxboat/fixuid/releases/download/v0.5/fixuid-0.5-linux-amd64.tar.gz -O /tmp/fixuid.gz
-if ! echo "caa7e0e4c88e1b154586a46c2edde75a23f9af4b5526bb11626e924204585050 /tmp/fixuid.gz" | sha256sum -c -; then
+wget https://github.com/boxboat/fixuid/releases/download/v0.5.1/fixuid-0.5.1-linux-amd64.tar.gz -O /tmp/fixuid.gz
+if ! echo "1077e7af13596e6e3902230d7260290fe21b2ee4fffcea1eb548e5c465a34800 /tmp/fixuid.gz" | sha256sum -c -; then
      echo "Hash failed"
      exit 1
 fi
